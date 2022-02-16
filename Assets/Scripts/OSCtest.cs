92009cecdfa0a669b9289ce9dfd97aac1055581c
MaxCoppen <44031065+mxcop@users.noreply.github.com>
2022-02-16 15:05:42 +0100

    OSC code

---
+++ Assets/Scripts/OSCtest.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityOSC;
+
+public class OSCtest : MonoBehaviour {
+
+    [SerializeField] private OSC osc;
+
+    private void Start() {
+        osc.SetAddressHandler("/Device/gravity", Receive);
+    }
+
+    void Receive(OscMessage msg) {
+        float x = msg.GetFloat(0);
+        Debug.Log($"[{msg.ip}]: " + x);
+    }
+
+}
