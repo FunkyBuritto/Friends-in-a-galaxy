23fcc438225464d7a1f61b3c84c612b6b442666d
FunkyBuritto <milanb043@gmail.com>
2022-02-17 14:39:56 +0100

    Boostercontroller osc integration

--- Assets/Scripts/Controllers/BoosterController.cs
+++ Assets/Scripts/Controllers/BoosterController.cs
@@ -5,20 +5,24 @@ using UnityOSC;
 
 public class BoosterController : MonoBehaviour
 {
-    [HideInInspector] public float inputValue = 0;
-
     public bool useKeyboard = false;
+    public float boostSpeed;
     public float rotationSpeed;
     public float maxRotation;
 
+    float inputValue = 0;
     bool boosting = false;
     bool addedUser = false;
+
+    Rigidbody2D shipBody;
+    Transform spriteTransform;
     OSCUser user;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        shipBody = GetComponentInParent<Rigidbody2D>();
+        spriteTransform = GetComponentsInChildren<Transform>()[1];
     }
 
     // Update is called once per frame
@@ -29,25 +33,30 @@ public class BoosterController : MonoBehaviour
             boosting = Input.GetKey(KeyCode.Space);
         }
         else {
-            if(!addedUser)
+            if (!addedUser)
+            {
                 user = OSCUser.GetDriver();
+                addedUser = true;
+            }
             else
             {
                 user.AddHook("gravity", setInput);
+                user.AddHook("touch", setTouch);
             }
         }
-
-        if (Mathf.Abs(inputValue) < 0.1)
-            return;
-
-        RotateBooster(inputValue);
     }
 
-    public void setInput(ArrayList list)
+    private void FixedUpdate()
     {
-        inputValue = (float)list[0];
-    }
+        if (boosting && useKeyboard) {
+            shipBody.AddForceAtPosition(transform.up.normalized * boostSpeed, spriteTransform.position, ForceMode2D.Impulse);
+        }
 
+        if (!(Mathf.Abs(inputValue) < 0.1))
+            RotateBooster(inputValue);
+    }
     public void RotateBooster(float value) { transform.Rotate(new Vector3(0, 0, -inputValue * rotationSpeed * Time.deltaTime)); }
 
+    public void setInput(ArrayList list) { inputValue = (float)list[0]; }
+    public void setTouch(ArrayList list) { shipBody.AddForceAtPosition(transform.up.normalized * boostSpeed, spriteTransform.position, ForceMode2D.Impulse); }
 }
