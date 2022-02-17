47be1944acc125c4edfd8ecf7e9e3c0aee6fa1fc
MaxCoppen <44031065+mxcop@users.noreply.github.com>
2022-02-17 12:52:20 +0100

    OSC Lobby System

---
+++ Assets/Scripts/OSC/OSCUser.cs
@@ -0,0 +1,25 @@
+﻿namespace UnityOSC
+{
+    [System.Serializable]
+    public class OSCUser
+    {
+        // Ip address of this user.
+        public string ip;
+
+        public OSCUser(string ip)
+        {
+            this.ip = ip;
+        }
+
+        /// <summary>
+        /// Add a hook to the OSC server for this specific user.
+        /// DO NOT ADD /Device/ infront of the <param name="address">address</param>.
+        /// </summary>
+        /// <param name="address">The OSC address {type}</param>
+        /// <param name="handler">The callback for when a message is recieved</param>
+        public void AddHook(string address, OscMessageHandler handler)
+        {
+            OSCHandler.AddUserHook("/Device/" + address, ip, handler);
+        }
+    }
+}
--- Assets/Scripts/OSCtest.cs
+++ Assets/Scripts/OSCtest.cs
@@ -1,17 +1,18 @@
 using UnityEngine;
 using UnityOSC;
 
-public class OSCtest : MonoBehaviour {
-
+public class OSCtest : MonoBehaviour
+{
     [SerializeField] private OSC osc;
 
-    private void Start() {
+    private void Start()
+    {
         osc.SetAddressHandler("/Device/gravity", Receive);
     }
 
-    void Receive(OscMessage msg) {
+    void Receive(OscMessage msg)
+    {
         float x = msg.GetFloat(0);
         Debug.Log($"[{msg.ip}]: " + x);
     }
-
 }
